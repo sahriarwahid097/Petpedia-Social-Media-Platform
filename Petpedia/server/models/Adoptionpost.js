7b1e847077a4fbf6abde46ceb4b4f56e0c4e1935
Sahriar Wahid Galib <sahriarwahid084@gmail.com>
2024-04-18 22:53:23 +0600

    Add files via upload

---
+++ Petpedia/server/models/Adoptionpost.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const adoptionpostSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Adoptionpost = mongoose.model(" Adoptionpost",adoptionpostSchema);
+
+export default Adoptionpost;
